33201ea7bfadd115f09a8503795da4699d59aa7b
zakdjahed <zxd234@miami.edu>
2025-01-29 18:02:02 -0500

    initial commit

---
+++ GetMyStuff.sh
@@ -0,0 +1,9 @@
+#!/bin/tcsh
+
+echo "User ID: `id -u`"
+echo "Primary Group ID: `id -g`"
+echo "All Groups: `id -Gn`"
+echo "Shell Path: $SHELL"
+echo "Home Directory: $HOME"
+echo "PATH Variable: $PATH"
+
